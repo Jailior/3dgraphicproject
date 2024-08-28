ea7c1ab312cda271eabec7fd054a0dd57d3e3c0d
Jailior <ahusseinse@yahoo.com>
2024-08-28 14:23:33 +0400

    working on adding texturing

--- 3dgraphicproject/3dgraphicproject.cpp
+++ 3dgraphicproject/3dgraphicproject.cpp
@@ -4,6 +4,14 @@
 #include <algorithm>
 using namespace std;
 
+struct vec2d
+{
+	float u = 0;
+	float v = 0;
+	float w = 1;
+};
+
+
 struct vec3d // a 3d vector structure
 {
 	float x = 0;
@@ -15,7 +23,7 @@ struct vec3d // a 3d vector structure
 struct triangle // a triangle, to be drawn, made up of 3 vertices each of which are vectors
 {
 	vec3d p[3];
-
+	vec2d t[3]; // texture
 	wchar_t sym;
 	short col;
 };
@@ -24,7 +32,7 @@ struct mesh // a grouping of triangles
 {
 	vector<triangle> tris;
 
-	bool LoadFromObjectFile(string sFilename)
+	bool LoadFromObjectFile(string sFilename, bool bHasTexture = false)
 	{
 		ifstream f(sFilename);
 		if (!f.is_open())
@@ -34,6 +42,7 @@ struct mesh // a grouping of triangles
 
 		// Local cache of vertices
 		vector<vec3d> verts;
+		vector<vec2d> texs;
 
 		while (!f.eof())
 		{
@@ -47,15 +56,54 @@ struct mesh // a grouping of triangles
 
 			if (line[0] == 'v')
 			{
-				vec3d v;
-				s >> junk >> v.x >> v.y >> v.z;
-				verts.push_back(v);
+				if (line[1] == 't')
+				{
+					vec2d v;
+					s >> junk >> junk >> v.u >> v.v;
+					texs.push_back(v);
+				}
+				else
+				{
+					vec3d v;
+					s >> junk >> v.x >> v.y >> v.z;
+					verts.push_back(v);
+				}
 			}
-			if (line[0] == 'f')
+			if (!bHasTexture)
 			{
-				int f[3];
-				s >> junk >> f[0] >> f[1] >> f[2];
-				tris.push_back({ verts[f[0] - 1], verts[f[1] - 1], verts[f[2] - 1] });
+				if (line[0] == 'f')
+				{
+					int f[3];
+					s >> junk >> f[0] >> f[1] >> f[2];
+					tris.push_back({ verts[f[0] - 1], verts[f[1] - 1], verts[f[2] - 1] });
+				}
+			}
+			else
+			{
+				if (line[0] == 'f')
+				{
+					s >> junk;
+
+					string tokens[6];
+					int nTokenCount = -1;
+
+
+					while (!s.eof())
+					{
+						char c = s.get();
+						if (c == ' ' || c == '/')
+							nTokenCount++;
+						else
+							tokens[nTokenCount].append(1, c);
+					}
+
+					tokens[nTokenCount].pop_back();
+
+
+					tris.push_back({ verts[stoi(tokens[0]) - 1], verts[stoi(tokens[2]) - 1], verts[stoi(tokens[4]) - 1],
+						texs[stoi(tokens[1]) - 1], texs[stoi(tokens[3]) - 1], texs[stoi(tokens[5]) - 1] });
+
+				}
 			}
 		}
 
@@ -88,6 +136,8 @@ private:
 	float fYaw;
 
 	float fTheta;
+
+	olcSprite* sprTex1;
 
 	vec3d Matrix_MultiplyVector(mat4x4 &m, vec3d &i)
 	{
@@ -270,13 +320,13 @@ private:
 		return v;
 	}
 
-	vec3d Vector_IntersectPlane(vec3d& plane_p, vec3d& plane_n, vec3d& lineStart, vec3d& lineEnd)
+	vec3d Vector_IntersectPlane(vec3d& plane_p, vec3d& plane_n, vec3d& lineStart, vec3d& lineEnd, float &t)
 	{
 		plane_n = Vector_Normalize(plane_n);
 		float plane_d = -Vector_DotProduct(plane_n, plane_p);
 		float ad = Vector_DotProduct(lineStart, plane_n);
 		float bd = Vector_DotProduct(lineEnd, plane_n);
-		float t = (-plane_d - ad) / (bd - ad);
+		t = (-plane_d - ad) / (bd - ad);
 		vec3d lineStartToEnd = Vector_Sub(lineEnd, lineStart);
 		vec3d lineToIntersect = Vector_Mul(lineStartToEnd, t);
 		return Vector_Add(lineStart, lineToIntersect);
@@ -297,18 +347,41 @@ private:
 		// If distance sign is positive, point lies on "inside" of plane
 		vec3d* inside_points[3];  int nInsidePointCount = 0;
 		vec3d* outside_points[3]; int nOutsidePointCount = 0;
+		vec2d* inside_tex[3]; int nInsideTexCount = 0;
+		vec2d* outside_tex[3]; int nOutsideTexCount = 0;
+
 
 		// Get signed distance of each point in triangle to plane
 		float d0 = dist(in_tri.p[0]);
 		float d1 = dist(in_tri.p[1]);
 		float d2 = dist(in_tri.p[2]);
 
-		if (d0 >= 0) { inside_points[nInsidePointCount++] = &in_tri.p[0]; }
-		else { outside_points[nOutsidePointCount++] = &in_tri.p[0]; }
-		if (d1 >= 0) { inside_points[nInsidePointCount++] = &in_tri.p[1]; }
-		else { outside_points[nOutsidePointCount++] = &in_tri.p[1]; }
-		if (d2 >= 0) { inside_points[nInsidePointCount++] = &in_tri.p[2]; }
-		else { outside_points[nOutsidePointCount++] = &in_tri.p[2]; }
+		if (d0 >= 0) { 
+			inside_points[nInsidePointCount++] = &in_tri.p[0]; 
+			inside_tex[nInsideTexCount++] = &in_tri.t[0]; 
+		}
+		else { 
+			outside_points[nOutsidePointCount++] = &in_tri.p[0]; 
+			outside_tex[nOutsideTexCount++] = &in_tri.t[0];
+		}
+
+		if (d1 >= 0) { 
+			inside_points[nInsidePointCount++] = &in_tri.p[1]; 
+			inside_tex[nInsideTexCount++] = &in_tri.t[1];
+		}
+		else { 
+			outside_points[nOutsidePointCount++] = &in_tri.p[1]; 
+			outside_tex[nOutsideTexCount++] = &in_tri.t[1];
+		}
+
+		if (d2 >= 0) { 
+			inside_points[nInsidePointCount++] = &in_tri.p[2]; 
+			inside_tex[nInsideTexCount++] = &in_tri.t[2];
+		}
+		else { 
+			outside_points[nOutsidePointCount++] = &in_tri.p[2]; 
+			outside_tex[nOutsideTexCount++] = &in_tri.t[2];
+		}
 
 		if (nInsidePointCount == 0)
 		{
@@ -338,11 +411,19 @@ private:
 
 			// The inside point is valid, so keep that...
 			out_tri1.p[0] = *inside_points[0];
+			out_tri1.t[0] = *inside_tex[0];
 
 			// but the two new points are at the locations where the 
 			// original sides of the triangle (lines) intersect with the plane
-			out_tri1.p[1] = Vector_IntersectPlane(plane_p, plane_n, *inside_points[0], *outside_points[0]);
-			out_tri1.p[2] = Vector_IntersectPlane(plane_p, plane_n, *inside_points[0], *outside_points[1]);
+			float t;
+			out_tri1.p[1] = Vector_IntersectPlane(plane_p, plane_n, *inside_points[0], *outside_points[0], t);
+			out_tri1.t[1].u = t * (outside_tex[0]->u - inside_tex[0]->u) + inside_tex[0]->u;
+			out_tri1.t[1].v = t * (outside_tex[0]->v - inside_tex[0]->v) + inside_tex[0]->v;
+			
+			
+			out_tri1.p[2] = Vector_IntersectPlane(plane_p, plane_n, *inside_points[0], *outside_points[1], t);
+			out_tri1.t[2].u = t * (outside_tex[0]->u - inside_tex[0]->u) + inside_tex[0]->u;
+			out_tri1.t[2].v = t * (outside_tex[0]->v - inside_tex[0]->v) + inside_tex[0]->v;
 
 			return 1; // Return the newly formed single triangle
 		}
@@ -365,14 +446,26 @@ private:
 			// intersects with the plane
 			out_tri1.p[0] = *inside_points[0];
 			out_tri1.p[1] = *inside_points[1];
-			out_tri1.p[2] = Vector_IntersectPlane(plane_p, plane_n, *inside_points[0], *outside_points[0]);
+			out_tri1.t[0] = *inside_tex[0];
+			out_tri1.t[1] = *inside_tex[1];
+
+			float t;
+			out_tri1.p[2] = Vector_IntersectPlane(plane_p, plane_n, *inside_points[0], *outside_points[0], t);
+			out_tri1.t[2].u = t * (outside_tex[0]->u - inside_tex[0]->u) + inside_tex[0]->u;
+			out_tri1.t[2].v = t * (outside_tex[0]->v - inside_tex[0]->v) + inside_tex[0]->v;
 
 			// The second triangle is composed of one of he inside points, a
 			// new point determined by the intersection of the other side of the 
 			// triangle and the plane, and the newly created point above
 			out_tri2.p[0] = *inside_points[1];
 			out_tri2.p[1] = out_tri1.p[2];
-			out_tri2.p[2] = Vector_IntersectPlane(plane_p, plane_n, *inside_points[1], *outside_points[0]);
+
+			out_tri2.t[0] = *inside_tex[1];
+			out_tri2.t[1] = out_tri1.t[2];
+
+			out_tri2.p[2] = Vector_IntersectPlane(plane_p, plane_n, *inside_points[1], *outside_points[0], t);
+			out_tri2.t[2].u = t * (outside_tex[0]->u - inside_tex[1]->u) + inside_tex[1]->u;
+			out_tri2.t[2].v = t * (outside_tex[0]->v - inside_tex[1]->v) + inside_tex[1]->v;
 
 			return 2; // Return two newly formed triangles which form a quad
 		}
@@ -420,8 +513,9 @@ public:
 	bool OnUserCreate() override
 	{
 		
-		meshCube.LoadFromObjectFile("mountains.obj");
-
+		//meshCube.LoadFromObjectFile("mountains.obj");
+		meshCube.LoadFromObjectFile("ArtisansHub.obj");
+		sprTex1 = new olcSprite(L"spritesheet.spr");
 
 		matProj = Matrix_MakeProjection(90.0f, (float)ScreenHeight() / (float)ScreenWidth(), 0.1f, 1000.0f);
 
@@ -492,6 +586,9 @@ public:
 			triTransformed.p[0] = Matrix_MultiplyVector(matWorld, tri.p[0]);
 			triTransformed.p[1] = Matrix_MultiplyVector(matWorld, tri.p[1]);
 			triTransformed.p[2] = Matrix_MultiplyVector(matWorld, tri.p[2]);
+			triTransformed.t[0] = tri.t[0];
+			triTransformed.t[1] = tri.t[1];
+			triTransformed.t[2] = tri.t[2];
 
 			vec3d normal, line1, line2;
 			
@@ -522,6 +619,9 @@ public:
 				triViewed.p[2] = Matrix_MultiplyVector(matView, triTransformed.p[2]);
 				triViewed.sym = triTransformed.sym;
 				triViewed.col = triTransformed.col;
+				triViewed.t[0] = triTransformed.t[0];
+				triViewed.t[1] = triTransformed.t[1];
+				triViewed.t[2] = triTransformed.t[2];
 
 				int nClippedTriangles = 0;
 				triangle clipped[2];
@@ -535,6 +635,21 @@ public:
 					triProjected.p[2] = Matrix_MultiplyVector(matProj, clipped[n].p[2]);
 					triProjected.col = clipped[n].col;
 					triProjected.sym = clipped[n].sym;
+					triProjected.t[0] = clipped[n].t[0];
+					triProjected.t[1] = clipped[n].t[1];
+					triProjected.t[2] = clipped[n].t[2];
+
+					triProjected.t[0].u = triProjected.t[0].u / triProjected.p[0].w;
+					triProjected.t[1].u = triProjected.t[1].u / triProjected.p[1].w;
+					triProjected.t[2].u = triProjected.t[2].u / triProjected.p[2].w;
+
+					triProjected.t[0].v = triProjected.t[0].v / triProjected.p[0].w;
+					triProjected.t[1].v = triProjected.t[1].v / triProjected.p[1].w;
+					triProjected.t[2].v = triProjected.t[2].v / triProjected.p[2].w;
+
+					triProjected.t[0].w = 1.0f / triProjected.p[0].w;
+					triProjected.t[1].w = 1.0f / triProjected.p[1].w;
+					triProjected.t[2].w = 1.0f / triProjected.p[2].w;
 
 					triProjected.p[0] = Vector_Div(triProjected.p[0], triProjected.p[0].w);
 					triProjected.p[1] = Vector_Div(triProjected.p[1], triProjected.p[1].w);
@@ -626,13 +741,181 @@ public:
 			// Draw the transformed, viewed, clipped, projected, sorted, clipped triangles
 			for (auto& t : listTriangles)
 			{
-				FillTriangle(t.p[0].x, t.p[0].y, t.p[1].x, t.p[1].y, t.p[2].x, t.p[2].y, t.sym, t.col);
-				//DrawTriangle(t.p[0].x, t.p[0].y, t.p[1].x, t.p[1].y, t.p[2].x, t.p[2].y, PIXEL_SOLID, FG_BLACK);
+				TexturedTriangle(t.p[0].x, t.p[0].y, t.t[0].u, t.t[0].v, t.t[0].w,
+					t.p[1].x, t.p[1].y, t.t[1].u, t.t[1].v, t.t[1].w,
+					t.p[2].x, t.p[2].y, t.t[2].u, t.t[2].v, t.t[2].w, sprTex1);
+
+				//FillTriangle(t.p[0].x, t.p[0].y, t.p[1].x, t.p[1].y, t.p[2].x, t.p[2].y, t.sym, t.col);
+				DrawTriangle(t.p[0].x, t.p[0].y, t.p[1].x, t.p[1].y, t.p[2].x, t.p[2].y, PIXEL_SOLID, FG_WHITE);
 			}
 		}
 
 		return true;
 	}
+
+	void TexturedTriangle(int x1, int y1, float u1, float v1, float w1,
+		int x2, int y2, float u2, float v2, float w2,
+		int x3, int y3, float u3, float v3, float w3,
+		olcSprite* tex)
+	{
+		if (y2 < y1)
+		{
+			swap(y1, y2);
+			swap(x1, x2);
+			swap(u1, u2);
+			swap(v1, v2);
+			swap(w1, w2);
+		}
+
+		if (y3 < y1)
+		{
+			swap(y1, y3);
+			swap(x1, x3);
+			swap(u1, u3);
+			swap(v1, v3);
+			swap(w1, w3);
+		}
+
+		if (y3 < y2)
+		{
+			swap(y2, y3);
+			swap(x2, x3);
+			swap(u2, u3);
+			swap(v2, v3);
+			swap(w2, w3);
+		}
+
+		int dy1 = y2 - y1;
+		int dx1 = x2 - x1;
+		float dv1 = v2 - v1;
+		float du1 = u2 - u1;
+		float dw1 = w2 - w1;
+
+		int dy2 = y3 - y1;
+		int dx2 = x3 - x1;
+		float dv2 = v3 - v1;
+		float du2 = u3 - u1;
+		float dw2 = w3 - w1;
+
+		float tex_u, tex_v, tex_w;
+
+		float dax_step = 0, dbx_step = 0,
+			du1_step = 0, dv1_step = 0,
+			du2_step = 0, dv2_step = 0,
+			dw1_step = 0, dw2_step = 0;
+
+		if (dy1) dax_step = dx1 / (float)abs(dy1);
+		if (dy2) dbx_step = dx2 / (float)abs(dy2);
+
+		if (dy1) du1_step = du1 / (float)abs(dy1);
+		if (dy1) dv1_step = dv1 / (float)abs(dy1);
+		if (dy1) dw1_step = dw1 / (float)abs(dy1);
+
+		if (dy2) du2_step = du2 / (float)abs(dy2);
+		if (dy2) dv2_step = dv2 / (float)abs(dy2);
+		if (dy1) dw2_step = dw2 / (float)abs(dy1);
+
+		if (dy1)
+		{
+			for (int i = y1; i <= y2; i++)
+			{
+				int ax = x1 + (float)(i - y1) * dax_step;
+				int bx = x1 + (float)(i - y1) * dbx_step;
+
+				float tex_su = u1 + (float)(i - y1) * du1_step;
+				float tex_sv = v1 + (float)(i - y1) * dv1_step;
+				float tex_sw = w1 + (float)(i - y1) * dw1_step;
+
+				float tex_eu = u1 + (float)(i - y1) * du2_step;
+				float tex_ev = v1 + (float)(i - y1) * dv2_step;
+				float tex_ew = w1 + (float)(i - y1) * dw2_step;
+
+				if (ax > bx)
+				{
+					swap(ax, bx);
+					swap(tex_su, tex_eu);
+					swap(tex_sv, tex_ev);
+					swap(tex_sw, tex_ew);
+				}
+
+				tex_u = tex_su;
+				tex_v = tex_sv;
+				tex_w = tex_sw;
+
+				float tstep = 1.0f / ((float)(bx - ax));
+				float t = 0.0f;
+
+				for (int j = ax; j < bx; j++)
+				{
+					tex_u = (1.0f - t) * tex_su + t * tex_eu;
+					tex_v = (1.0f - t) * tex_sv + t * tex_ev;
+					tex_w = (1.0f - t) * tex_sw + t * tex_ew;
+					Draw(j, i, tex->SampleGlyph(tex_u / tex_w, tex_v / tex_w), tex->SampleColour(tex_u, tex_v));
+
+					t += tstep;
+				}
+
+			}
+		}
+
+		dy1 = y3 - y2;
+		dx1 = x3 - x2;
+		dv1 = v3 - v2;
+		du1 = u3 - u2;
+		dw1 = w3 - w2;
+
+		if (dy1) dax_step - dx1 / (float)abs(dy1);
+		if (dy2) dbx_step - dx2 / (float)abs(dy2);
+
+		du1_step = 0, dv1_step - 9;
+		if (dy1) du1_step = du1 / (float)abs(dy1);
+		if (dy1) dv1_step = dv1 / (float)abs(dy1);
+		if (dy1) dw1_step = dw1 / (float)abs(dy1);
+
+		for (int i = y2; i <= y3; i++)
+		{
+			int ax = x2 + (float)(i - y2) * dax_step;
+			int bx = x1 + (float)(i - y1) * dbx_step;
+
+			float tex_su = u2 + (float)(i - y2) * du1_step;
+			float tex_sv = v2 + (float)(i - y2) * dv1_step;
+			float tex_sw = w2 + (float)(i - y2) * dw1_step;
+
+			float tex_eu = u1 + (float)(i - y2) * du2_step;
+			float tex_ev = v1 + (float)(i - y2) * dv2_step;
+			float tex_ew = w1 + (float)(i - y2) * dw2_step;
+
+			if (ax > bx)
+			{
+				swap(ax, bx);
+				swap(tex_su, tex_eu);
+				swap(tex_sv, tex_ev);
+				swap(tex_sw, tex_ew);
+			}
+
+			tex_u = tex_su;
+			tex_v = tex_sv;
+			tex_w = tex_sw;
+
+			float tstep = 1.0f / ((float)(bx - ax));
+			float t = 0.0f;
+
+			for (int j = ax; j < bx; j++)
+			{
+				tex_u = (1.0f - t) * tex_su + t * tex_eu;
+				tex_v = (1.0f - t) * tex_sv + t * tex_ev;
+				tex_w = (1.0f - t) * tex_sw + t * tex_ew;
+
+				Draw(j, i, tex->SampleGlyph(tex_u, tex_v), tex->SampleColour(tex_u, tex_v));
+
+				t += tstep;
+			}
+
+		}
+
+
+	}
+
 };
 
 int main(){
